5e09ee49dfe5d8dd8cd8a179e999e8e3fdf1221e
Petar <google1999@abv.bg>
2016-08-04 22:52:49 +0300

    Added LevelManager class

---
+++ level_management/LevelManager.cpp
@@ -0,0 +1,72 @@
+#include "LevelManager.h"
+
+int LevelManager::currentLevel;
+
+#define LEVELS_PATH "Resources/Levels/"
+
+bool LevelManager::FileIsExisting(string filename)
+{
+    ifstream tmp(filename);
+    return (bool)tmp;
+}
+
+void LevelManager::InitCurrentLevel()
+{
+    ifstream level((string)LEVELS_PATH + "current.level");
+    level >> LevelManager::currentLevel;
+    level.close();
+}
+
+int LevelManager::GetCurrentLevel() { return currentLevel; }
+
+bool LevelManager::LoadLevel(int lvl)
+{
+    string fileToOpen = (string)LEVELS_PATH + to_string(lvl) + ".level";
+    if(LevelManager::FileIsExisting(fileToOpen))
+    {
+        ifstream level(fileToOpen);
+        if(level.is_open())
+        {
+            string line;
+            int line_num = 0;
+
+            for(std::string line; getline(level, line); )
+            {
+                //Add the number of the aliens in the AlienManager class
+                if(line_num == 0)
+                {
+                    level >> AliensManager::number;
+                }
+
+                //Add movement speed of the aliens in the AlienManager class
+                else if(line_num == 1)
+                {
+                    level >> AliensManager::movementSpeed;
+                }
+
+                //Add the shooting power coefficient of the aliens in the AlienManager class
+                else if(line_num == 2)
+                {
+                    level >> AliensManager::shootingPowerCoefficient;
+                }
+
+                //Read the aliens
+                else
+                {
+                    int type, pos_x, pos_y;
+                    level >> type >> pos_y >> pos_x;
+                    pos_x *= System::Screen::Width / 12;
+                    pos_y *= System::Screen::Height / 7;
+                    AliensManager::AddNewAlien(type, pos_x, pos_y);
+                }
+
+                line_num++;
+            }
+
+            level.close();
+            return true;
+        }
+        else return false;
+    }
+    else return false;
+}
